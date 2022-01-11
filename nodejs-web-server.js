e1572cb31afef199822bb7b21c4b02504324566f
Mehmet Ali ÇAKIR <32022785+mehmetalicakir@users.noreply.github.com>
2022-01-11 08:40:56 +0300

    Add comments

--- nodejs-web-server.js
+++ nodejs-web-server.js
@@ -1,14 +1,10 @@
-const http = require('http');
-const port = 3000;
-const server = http.createServer((req, res) => {
+const http = require('http'); // Load http module
+const port = 3000; // Define the port number
+const server = http.createServer((req, res) => { // Configure http server
     console.log("Request sent.");
     res.write("<h1>Welcome to the server</h1>");
     res.end();
 });
-
-
-
-
-server.listen(port, () => {
+server.listen(port, () => { //Listen the server
     console.log(`Server running on port ${port}`);
 });
